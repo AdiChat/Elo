909ae1dece26412edca6f15479d1e9ff18d51fd8
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2016-06-26 00:25:50 +0530

    2 player support

--- src/Game.java
+++ src/Game.java
@@ -4,4 +4,9 @@ class Game
     Private ArrayList<Player> players;
     Private int countPlayers;
     Private Mode gameMode;
+    
+    public Game()
+    {
+        countPlayers = 2; // Supporting 2 players currently
+    }
 }
