568bf4eb9356ffa91c96dffec096b507e3aa5870
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2016-06-27 13:43:14 +0530

    Game :angel:

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,36 @@
 class Game
 {
-    Private enum Mode { CHESS, LODO }
-    Private ArrayList<Player> players;
-    Private int countPlayers;
-    Private Mode gameMode;
+    private String gameName;
+    private ArrayList<Player> players;
+    private int countPlayers;
+    private int playerWon;
     
-    public Game()
+    Game()
     {
-        countPlayers = 2; // Supporting 2 players currently
+        countPlayers = 2;
+        gameName = "";
+        players = new ArrayList<Player>(countPlayers);
     }
+    
+    public void setGameName(String Name)
+    {
+        gameName = Name;
+    }
+    
+    public void setPlayers(int players)
+    {
+        countPlayers = players;
+    }
+    
+    public void addPlayer(Player player)
+    {
+        players.add(player);
+    }
+    
+    public void setWinner(int winner)
+    {
+        playerWon = winner;
+    }
+    
+    
 }
